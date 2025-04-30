827d2d13786f01c9ff1b5724909ac4736b58956e
Muhammad Shoaib <165574655+ShoaibFS@users.noreply.github.com>
2025-04-30 23:51:57 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 // src/App.js
+// src/App.js
+//import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          {/* Main landing page */}
+          <Route path="/" element={<Home />} />
+
+
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
