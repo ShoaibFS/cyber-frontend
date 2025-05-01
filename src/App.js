b6dfab662931e3865fccb10896594c50f6b1394e
Muhammad Shoaib <165574655+ShoaibFS@users.noreply.github.com>
2025-05-01 11:22:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 // src/App.js
 //import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import './App.css';
 function App() {
